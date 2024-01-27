6e005bfbf741ebf302cbe06a4bccca6840c1dda2
Aptech Student <student@aahn02.edu>
2024-01-27 09:37:59 +0700

    9: 37

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() async {
   // UserCtrl.insertTable(Users(1,'tuan','bt','12345',1,'tuan@gmail.com', '', '012345678', 'hn','', 1,1));
   // UserCtrl.insertTable(Users(2,'nguyen','th','12345',1,'nguyen@gmail.com', '', '012345678', 'hn','', 0,1));
   // UserCtrl.insertTable(Users(3,'kiet','bt','12345',1,'kiet@gmail.com', '', '012345678', 'hn','', 0,1));
-  listUser = await UserCtrl.getListTable();
+  //listUser = await UserCtrl.getListTable();
   // print(listUser.toString());
   //  listUser = await UserCtrl.findByName('nguyen');
   //   print(listUser.toString());
